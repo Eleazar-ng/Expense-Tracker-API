51fb4060c6068885f27c1affc4333ababc9b7ef7
Edward Eleazar <eleazaredward@gmail.com>
2025-12-04 23:44:30 +0100

    init: initialized project server

---
+++ server.js
@@ -0,0 +1,14 @@
+
+import express from "express";
+import "dotenv/config"
+
+const server = express();
+const PORT = process.env.PORT || 8084;
+
+// Start server
+server.listen(PORT, (err)=> {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`\nExpense Tracker API server running on http://localhost:${PORT}`);
+})
