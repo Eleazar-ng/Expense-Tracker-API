fb2508a34d9228b5b55942eed74ef83885b84374
Edward Eleazar <eleazaredward@gmail.com>
2026-01-17 15:05:38 +0100

    refactor(error-handler): updated error handler implementation

--- src/utils/error.handler.ts
+++ src/utils/error.handler.ts
@@ -1,15 +1,16 @@
 
 import { Request, Response, NextFunction } from "express";
 import { env } from "../config/env";
+import { CustomError } from "../errors";
+import { error } from "./api.response";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
+  if(err instanceof CustomError){
+    return error(res, err.serializeErrors()[0]?.message, err.statusCode);
+  }
 
-  res.status(500).json({
-    error: true,
-    message: env.NODE_ENV == "production" 
-      ? "Internal Server Error"
-      : err.message,
-    data: null,
-  })
+  const errMessage = env.NODE_ENV == "production" ? "Internal Server Error" : err.message
+
+  return error(res, errMessage, 500);
 }
